d47713df757928ab0a9fe307c2b0ed5bbb9ea679
Arash Hatami <hatamiarash7@gmail.com>
2020-04-13 15:45:08 +0430

    print info

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ def count_file_lines(json_file=""):
     Read lines before using multi-threads to do data importing I/O jobs
     """
     num_lines = 0
+
+    print("Count file lines ...")
+
     with open(json_file, encoding="utf8") as f:
         for _ in f:
             num_lines += 1
