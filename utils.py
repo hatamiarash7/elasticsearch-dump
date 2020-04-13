9808023373eb88567ce2b99e41b14015744690c1
Arash Hatami <hatamiarash7@gmail.com>
2020-04-13 15:34:51 +0430

    code cleanup

--- utils.py
+++ utils.py
@@ -3,10 +3,8 @@ def count_file_lines(json_file=""):
     # TODO 
     Read lines before using multi-threads to do data importing I/O jobs
     """
-
     num_lines = 0
     with open(json_file, encoding="utf8") as f:
-        for line in f:
+        for _ in f:
             num_lines += 1
     return num_lines
-
